575f7fc0c6fec1914ac1a5c8f5f587ac85ec862a
Victor Manuel Pinto Rico <k7victor@gmail.com>
2019-05-10 11:32:32 +0200

    Create VersionToIssueLink.groovy

---
+++ VersionToIssueLink.groovy
@@ -0,0 +1,53 @@
+import com.atlassian.jira.component.ComponentAccessor
+import com.atlassian.jira.event.type.EventDispatchOption
+import com.atlassian.jira.issue.Issue
+import com.atlassian.jira.issue.IssueManager
+import com.atlassian.jira.issue.MutableIssue
+import com.atlassian.jira.project.version.Version
+import com.atlassian.jira.project.version.VersionManager
+import com.atlassian.jira.event.issue.AbstractIssueEventListener
+import com.atlassian.jira.event.issue.IssueEvent
+import com.atlassian.jira.issue.link.IssueLink;
+import com.atlassian.jira.issue.link.IssueLinkManager
+import com.atlassian.jira.issue.link.IssueLinkTypeManager 
+import org.apache.log4j.Level
+
+log.setLevel(Level.DEBUG)
+
+def issueLinkManager = ComponentAccessor.getComponent(IssueLinkManager) 
+def versionManager = ComponentAccessor.getVersionManager()
+def issueManager = ComponentAccessor.getComponent(IssueManager.class)
+def currentUser = ComponentAccessor.getJiraAuthenticationContext().getLoggedInUser()
+def issueLinkTypeManager = ComponentAccessor.getComponent(IssueLinkTypeManager)
+def relatesLinkTypeName = "InFixVersion" 
+def relateLinkType = issueLinkTypeManager.getIssueLinkTypesByName(relatesLinkTypeName) 
+def linkedIssuesKeys = []
+
+Issue updatedIssue = event.getIssue()
+
+List<IssueLink> allOutIssueLink = ComponentAccessor.getIssueLinkManager().getOutwardLinks(updatedIssue.getId())
+
+allOutIssueLink.each{
+    if (it instanceof IssueLink) {
+        if (it.getIssueLinkType().getId().compareTo((Long) relateLinkType[0].id) ){
+                    linkedIssuesKeys.add(it)
+        }
+    }
+}
+
+log.debug("linkedIssuesKeys" + linkedIssuesKeys)
+
+Collection<Version> fixVersions = new ArrayList<Version>()
+fixVersions = updatedIssue.getFixVersions()
+fixVersions.each {   
+    if (it instanceof Version) {
+        Collection<Issue> issueInVersion = versionManager.getIssuesWithFixVersion(it)
+        issueInVersion.each { 
+			if (it instanceof Issue) {                 
+                issueLinkManager.createIssueLink((Long) updatedIssue.getId(), (Long) it.getId(), (Long) relateLinkType[0].id, (Long) 1, currentUser) 
+                
+            }
+   		 }	
+	}
+}
+
